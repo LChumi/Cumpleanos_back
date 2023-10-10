78b9e0784b6f9251d78ae6b5fc1a50ff165c6528
LChumi <luischumi.9@gmail.com>
2023-10-10 16:42:06 -0500

    feat (ImageController): se agrega la funcion de buscar imagenes

---
+++ web-api/src/main/java/com/cumpleanos/webapi/controller/ImageController.java
@@ -0,0 +1,19 @@
+package com.cumpleanos.webapi.controller;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.FileSystem;
+
+@RestController
+@RequestMapping("/bodegas")
+@CrossOrigin("*")
+public class ImageController {
+
+    @GetMapping(value = "/images/{imageName}",produces = "image/jpeg")
+    public FileSystemResource getImagen(@PathVariable String imageName){
+        String imagePath = "C:/Users/Luis/Pictures/Imagenes Post Cumpleaños/"+imageName;
+        FileSystemResource resource=new FileSystemResource(imagePath);
+        return resource;
+    }
+}
